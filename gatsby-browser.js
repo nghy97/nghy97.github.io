17eb2b282479f57d10171e67867213f6467d6f57
DONGHYUK <ehdgur920907@gmail.com>
2019-11-28 11:59:29 +0900

    add code highlighting

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,5 @@
 // custom typefaces
-import "typeface-montserrat"
-import "typeface-merriweather"
+import 'typeface-montserrat';
+import 'typeface-merriweather';
+require('prismjs/themes/prism-solarizedlight.css');
+require('prismjs/plugins/command-line/prism-command-line.css');
